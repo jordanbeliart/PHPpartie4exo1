3d7005ee9b13fc8ce33403111e2e2a303b5d1d12
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-03 17:02:35 +0100

    exo1

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
-/* déclaration d'une variable 'sentence' et d'une variable 'length' qui va
-calculer le nombre de caractères qu'il y a dans la variable 'sentence' grâce à
-la function 'strlen()' */
-$sentence = 'Combien y a-t-il de caractères dans cette phrase ?';
-$length = strlen($sentence);
+$number = 8;
+/* déclaration d'une fonction "cube" qui va calculer le cube
+ de la variable 'number' indiquée entre () pour ensuite retourner le résultat */
+function cube($number){
+  $calcul = $number * $number * $number;
+  return $calcul;
+}
+$result = cube($number);
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8" />
-    <title>PHP partie 4 exo 1</title>
+    <title>PHP partie4 exo 1</title>
   </head>
   <body>
-    <?php
-      echo $sentence . '<br />' . 'La phrase ci-dessus comporte ' . $length . ' caractères.';
-    ?>
+    <p>
+      <?php echo $result; ?>
+    </p>
   </body>
 </html>
